2b2a916b84eac228c3a46fb7aeaa31b25cb3e590
SHAKIL <sk2448868@gmail.com>
2025-08-12 23:11:49 +0600

    add

--- NEW/load_data.py
+++ NEW/load_data.py
@@ -2,11 +2,9 @@ from datasets import load_dataset
 import pandas as pd
 
 
-# Load only the first 3000 rows
-ds = load_dataset(
-    "ajibawa-2023/Children-Stories-Collection",
-    split="train[:1000]"
-)
+from datasets import load_dataset
+
+ds = load_dataset("asoria/children-stories-dataset")
 # Suppose it has a 'train' split:
 df = ds["train"].to_pandas()
 
